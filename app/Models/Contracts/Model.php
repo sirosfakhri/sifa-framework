98a5ac5cc9aec0c2c4fc507e1f9dc9ed21dee835
Siros Fakhri <siros.fakhri@gmail.com>
2023-03-03 17:56:30 +0330

    Add Base Model

---
+++ app/Models/Contracts/Model.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Sifa\App\Models\Contracts;
+
+abstract class Model implements CRUD
+{
+    protected string $connection;
+
+    protected string $table;
+
+    protected string $primaryKey = 'id';
+
+    protected int $pageSize = 10;
+
+    protected array $attribute = [];
+
+    protected function __construct()
+    {
+    }
+
+    protected function getAttribute($key){
+        if (!$key || !array_key_exists($key, $this->attribute))
+            return null;
+
+        return $this->attribute[$key];
+    }
+
+}
